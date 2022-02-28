926d8ef58918c8124f927513b59b483febe7da38
DiSLucky7 <nagyklevi@gmail.com>
2022-02-28 14:26:26 +0100

    Task 1 part 2 done

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,17 @@
 function manipulateDom() {
-    // 
+    const quotes = document.querySelectorAll('.quote');
+    quotes.forEach(quote => {
+        quote.style.borderColor = 'blue';
+        quote.style.backgroundColor = 'lightblue';
+        quote.style.fontStyle = 'italic';
+    })
 };
 
 export { manipulateDom };
+
+
+// A következőképpen kell működnie a függvénynek:
+// - Megkeres az oldalon minden quote osztályhoz tartozó elemet, és három stílust állít be rájuk JavaScript segítségével: 
+//   - a körvonaluk legyen kék, 
+//   - a hátterük világoskék 
+//   - és az általuk használt betűtípus dőlt.

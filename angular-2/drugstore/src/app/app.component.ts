1b26f10496be50c454b1980600a45ed7041bb8a8
DiSLucky7 <nagyklevi@gmail.com>
2022-02-28 14:52:25 +0100

    Task 2 part 1 & 2 done

--- angular-2/drugstore/src/app/app.component.ts
+++ angular-2/drugstore/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DrugService } from './service/drug.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drugstore';
+
+  constructor(
+    private drugService: DrugService,
+  ) {
+    this.drugService.getAll().subscribe(drugs => console.log(drugs))
+  }
 }
---
+++ angular-2/drugstore/src/app/model/drug.ts
@@ -0,0 +1,21 @@
+export class Drug {
+  id: number = 1;
+  name: string = "Escitalopram Oxalate";
+  brand: string = "Escitalopram";
+  company?: string = "Major Pharmaceuticals";
+  barcode: string = "23-537-0123";
+  stock: number= 26;
+}
+
+
+// const drugs = [{
+//   "id": 1,
+//   "name": "Escitalopram Oxalate",
+//   "brand": "Escitalopram",
+//   "company": "Major Pharmaceuticals",
+//   "barcode": "23-537-0123",
+//   "stock": 26
+// }];
+// ```
+// - A company ne legyen kötelező.
+// - Alapértelmezett értékként add meg az itt látható adatokat.
